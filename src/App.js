476b8a3826eea2219599b9de398be5a61142b8f4
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-09 12:25:41 +0100

    Set up API url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
@@ -11,7 +12,7 @@ function App() {
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}?client_id=1QjNo-Q66PQwa_CcxJgzoINrlovwnwc1Ik8kPzDuuHU`;
+    url = `${mainUrl}${clientID}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
